49b2f705f99ebe1fb9a80012d4c201f5ee0a0b47
fderose <fderose@guidewire.com>
2014-12-05 16:54:11 -0800

    Add script to load books data. Add initial version of logic to PersonalizeFunction to calculate boost.

--- data/loadbooks.sh
+++ data/loadbooks.sh
@@ -1,8 +1,8 @@
 URL=http://localhost:8983/solr/update/json
 curl $URL -H 'Content-type:application/json' -d '
 [
-  {"id":"1","title":"The British Bond Market","author":"George Banks"},
-  {"id":"2","title":"The Name is Bond","author":"Ian Fleming"}
+  {"id":"1","title":"The British Bond Market","author":"George Banks","category":"financial"},
+  {"id":"2","title":"The Name is Bond","author":"Ian Fleming","category":"espionage"}
 ]'
 curl $URL?commit=true
 
--- src/com/guidewire/solr/PersonalizeFunction.java
+++ src/com/guidewire/solr/PersonalizeFunction.java
@@ -6,27 +6,62 @@ import org.apache.lucene.queries.function.ValueSource;
 import org.apache.lucene.queries.function.docvalues.IntDocValues;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class PersonalizeFunction extends ValueSource {
 
-  static {
-    System.out.println("I can set a breakpoint here to see when the class is loaded");
-  }
-
   protected final String user;
   protected final ValueSource inputValueSource;
-  
+
+  private enum CategoryType {
+    financial,
+    espionage,
+    computer,
+    romance
+  }
+
+  // The profiles data store is a map from user name to a map from category type to the number of times the user has bought
+  // an item in that category. The more items the user has bought in the category, the higher the boost should be.
+  private final Map<String, Map<CategoryType,Integer>> profiles = new HashMap<>();
+
+  private void loadProfiles() {
+    Map<CategoryType, Integer> categoryMap = new HashMap<>();
+    categoryMap.put(CategoryType.espionage, 100);
+    categoryMap.put(CategoryType.computer, 20);
+    profiles.put("Peter", categoryMap);
+
+    categoryMap = new HashMap<>();
+    categoryMap.put(CategoryType.financial, 75);
+    categoryMap.put(CategoryType.computer, 10);
+    categoryMap.put(CategoryType.romance, 100);
+    profiles.put("Frank", categoryMap);
+  }
+
   public PersonalizeFunction(String user, ValueSource inputValueSource) {
     this.user = user;
     this.inputValueSource = inputValueSource;
+    loadProfiles();
   }
 
   @Override
-  public FunctionValues getValues(Map map, AtomicReaderContext atomicReaderContext) throws IOException {
+  public FunctionValues getValues(final Map map, final AtomicReaderContext atomicReaderContext) throws IOException {
+
+    final FunctionValues inputValues = inputValueSource.getValues(map, atomicReaderContext);
+
     return new IntDocValues(this) {
       @Override
       public int intVal(int doc) {
+        if (profiles.containsKey(user)) {
+          Map<CategoryType, Integer> categoryMap = profiles.get(user);
+          String inputValue = inputValues.strVal(doc);
+          if (inputValue != null) {
+            CategoryType categoryType = CategoryType.valueOf(inputValue);
+            if (categoryMap.containsKey(categoryType)) {
+              return categoryMap.get(categoryType);
+            }
+          }
+        }
         return 0;
       }
     };
